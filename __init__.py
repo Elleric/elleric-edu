a3f95c33cae7d19943da8d4e71f5826c03296b2c
Elleric <58402719+Elleric@users.noreply.github.com>
2020-07-09 08:39:06 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from flask import Flask  # из библиотеки flask импортируем КЛАСС Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
+
+App = Flask(__name__)
+App.config.from_object(Config)
+db = SQLAlchemy(App)
+migrate = Migrate(App, db)
+login = LoginManager(App)
+
+from habrClone import routes, models, errors  # импортировали маршруты
